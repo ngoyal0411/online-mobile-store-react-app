3bc37a175db7f389ae8a6fdcea0dd8c550dd6991
ngoyal0411 <ngoyal0411@gmail.com>
2020-05-19 22:49:04 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React  from 'react';
+import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.js";
+import App from "./components/App";
+
+render(<App />, document.getElementById("app"));
